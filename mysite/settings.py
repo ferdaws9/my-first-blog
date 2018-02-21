7166a736b15d7a35b6052b1752320785c3e163a7
ferdaws <ferchichiferdaws@gmail.com>
2018-02-21 15:02:33 +0100

    additional files and changes for Heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -23,9 +24,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'w1k3aa8f$y&w)4-7vp5k%tsue=yc63vhnsqtxow1mdp3_%#(ot'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'ferdaws.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'ferdaws.pythonanywhere.com','.herokuapp.com']
 
 
 # Application definition
@@ -74,23 +76,27 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-#DATABASES = {
- #   'default': {
-  #      'ENGINE': 'django.db.backends.postgresql_psycopg2',
-   #     'NAME': 'djangogirls',
-    #    'USER': 'ferdaws',
-     #   'PASSWORD': 'ferfer29281994',
-      #  'HOST': 'localhost',
-       # 'PORT': '',
-    #}
-#}
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'djangogirls',
+        'USER': 'ferdaws',
+        'PASSWORD': 'ferfer29281994',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+#DATABASES = {
+ #   'default': {
+  #      'ENGINE': 'django.db.backends.sqlite3',
+   #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #}
+#}
 
 
 # Password validation
